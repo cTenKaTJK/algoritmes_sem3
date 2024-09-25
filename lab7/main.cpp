c2483f1b2cd34d22991cfbfe1fab93d1a2751e66
cTenKaTJK <99907848+cTenKaTJK@users.noreply.github.com>
2024-09-26 00:28:27 +0200

    SOOOOORT

--- lab7/main.cpp
+++ lab7/main.cpp
@@ -1,4 +1,4 @@
-//	сортировка Шелла
+//	СЃРѕСЂС‚РёСЂРѕРІРєР° РЁРµР»Р»Р°
 #include <iostream>
 
 
@@ -11,7 +11,7 @@ int main() {
 	for (size_t i = 0; i < len; i++)
 		std::cin >> arr[i];
 
-	for (int step = len / 2; step > 0; step /= 2) {	//	шаг уменьшается в два раза
+	for (int step = len / 2; step > 0; step /= 2) {	//	С€Р°Рі СѓРјРµРЅСЊС€Р°РµС‚СЃСЏ РІ РґРІР° СЂР°Р·Р°
 		for (int i = step; i < len; ++i) {
 			for (int j = i - step; j >= 0 && arr[j] > arr[j + step]; j -= step)
 				std::swap(arr[j], arr[j + step]);
