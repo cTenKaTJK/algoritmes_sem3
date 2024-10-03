f5eac8a33ceb6646890f363177a1a636dc5b9a19
cTenKaTJK <99907848+cTenKaTJK@users.noreply.github.com>
2024-10-03 10:44:03 +0200

    SOOOORT

--- lab10/main.cpp
+++ lab10/main.cpp
@@ -1,6 +1,5 @@
+// сортировка слиянием
 #include <iostream>
-#include <algorithm>
-#include <cstddef>
 #include <iterator>
 #include <memory>
 
@@ -10,57 +9,59 @@ void sort(int array[], std::size_t size) {
         std::size_t const left_size = size / 2;
         std::size_t const right_size = size - left_size;
 
+        // дробление массива посредством рекурсивного вызова процедуры
         sort(&array[0], left_size);
         sort(&array[left_size], right_size);
 
-        std::size_t lidx = 0, ridx = left_size, idx = 0;
+        std::size_t left = 0, right = left_size, curr_id = 0;
         std::unique_ptr<int[]> tmp_array(new int[size]);
 
-        while (lidx < left_size || ridx < size) {
-            if (array[lidx] < array[ridx]) {
-                tmp_array[idx++] = std::move(array[lidx]);
-                lidx++;
+        // упорядоченная запись элементов двух раздробленных массивов в один
+        while (left < left_size || right < size) {
+            if (array[left] < array[right]) {
+                tmp_array[curr_id++] = std::move(array[left]);
+                left++;
             }
             else {
-                tmp_array[idx++] = std::move(array[ridx]);
-                ridx++;
+                tmp_array[curr_id++] = std::move(array[right]);
+                right++;
             }
 
-            if (lidx == left_size) {
-                std::copy(std::make_move_iterator(&array[ridx]),
+            if (left == left_size) {
+                std::copy(std::make_move_iterator(&array[right]),
                     std::make_move_iterator(&array[size]),
-                    &tmp_array[idx]);
+                    &tmp_array[curr_id]);
                 break;
             }
-            if (ridx == size) {
-                std::copy(std::make_move_iterator(&array[lidx]),
+            if (right == size) {
+                std::copy(std::make_move_iterator(&array[left]),
                     std::make_move_iterator(&array[left_size]),
-                    &tmp_array[idx]);
+                    &tmp_array[curr_id]);
                 break;
             }
         }
 
         std::copy(std::make_move_iterator(tmp_array.get()),
             std::make_move_iterator(&tmp_array[size]),
-            array);
+            array); // копирование отсортированной части в исходный массив
     }
 }
 
 
 int main() {
 
-	size_t len;
-	std::cin >> len;
-	int* arr = new int[len];
+    size_t len;
+    std::cin >> len;
+    int* arr = new int[len];
 
-	for (size_t i = 0; i < len; i++)
-		std::cin >> arr[i];
+    for (size_t i = 0; i < len; i++)
+        std::cin >> arr[i];
 
     sort(arr, len);
 
-	for (size_t i = 0; i < len; i++)
-		std::cout << arr[i] << ' ';
-	std::cout << std::endl;
+    for (size_t i = 0; i < len; i++)
+        std::cout << arr[i] << ' ';
+    std::cout << std::endl;
 
-	return 0;
+    return 0;
 }
