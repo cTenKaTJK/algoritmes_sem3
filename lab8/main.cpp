8f379daeef8b136a9d780182b238b15206306833
cTenKaTJK <99907848+cTenKaTJK@users.noreply.github.com>
2024-12-05 10:48:16 +0200

    WE RE SOOOO BASK

--- lab8/main.cpp
+++ lab8/main.cpp
@@ -1,95 +1,56 @@
-def brackets_check(line):
-    brackets = []
-    bracket_pairs = {')': '(', '}': '{', ']': '['}
-    for sym in line:
-        if sym in bracket_pairs.values():
-            brackets.append(sym)
-        elif sym in bracket_pairs.keys():
-            if brackets and brackets[-1] == bracket_pairs[sym]:
-                brackets.pop()
-            else:
-                return False
-
-    return len(brackets) == 0
+// поразрядная сортировка
+#include <iostream>
+#include <vector>
 
 
-def to_polish(expression):
-    opers = {'+': 1, '-': 1, '*': 2, '/': 2}    #определение приоритета оператора
-    output, operators, i = [], [], 0
+int nl(int x) {	// число разрядов в числе
+	size_t len = 0;
+	while (x > 0) {
+		x /= 10;
+		len++;
+	}
+	return len;
+}
 
-    while i < len(expression):
-        print(*output, '\n', *operators, '\n---------------\n')
-        sym = expression[i]
-
-        if sym == '-' and (i == 0 or expression[i - 1] in '(-+*/'):
-            number = sym
-            i += 1
-            while i < len(expression) and expression[i].isdigit():
-                number += expression[i]
-                i += 1
-            output.append(number)
-            continue
-
-        if sym.isdigit():
-            number = sym
-            while i + 1 < len(expression) and expression[i + 1].isdigit():
-                i += 1
-                number += expression[i]
-            output.append(number)
-        elif sym in opers:
-            while ((operators and operators[-1] in opers) and (opers[operators[-1]] >= opers[sym])):
-                output.append(operators.pop())
-            operators.append(sym)
-        elif sym == '(':
-            operators.append(sym)
-        elif sym == ')':
-            while operators and operators[-1] != '(':
-                output.append(operators.pop())
-        i += 1
-
-    while operators:
-        output.append(operators.pop())
-
-    return output
+int digt(int x, size_t place) {	// цифра в разряде
+	for (size_t i = 1; i < place; i++) {
+		if (x > 0) x /= 10;
+		else return 0;
+	}
+	return x % 10;
+}
 
 
-def evaluate_pol(rpn):
-    stack = []
-    for sigh in rpn:
-        if sigh.lstrip('-').isdigit():
-            stack.append(int(sigh))
-        else:
-            b = stack.pop()
-            a = stack.pop()
+int main() {
 
-            if sigh == '+':
-                stack.append(a + b)
-            elif sigh == '-':
-                stack.append(a - b)
-            elif sigh == '*':
-                stack.append(a * b)
-            elif sigh == '/':
-                if b == 0:
-                    raise ZeroDivisionError("деление на ноль")
-                stack.append(a / b)
+	size_t len;
+	std::cin >> len;
+	int* arr = new int[len];
 
-    return stack[0]
+	for (size_t i = 0; i < len; i++)
+		std::cin >> arr[i];
 
-
-def evaluate(expression):
-    expression = expression.replace('=', '')
-
-    if not brackets_check(expression):
-        print("Некорректные скобки в выражении")
-        return
-
-    try:
-        rpn = to_polish(expression)
-        result = evaluate_pol(rpn)
-        print("Результат:", result)
-    except ZeroDivisionError:
-        print(ZeroDivisionError)
+	size_t max_len = 0;
+	for (size_t i = 0; i < len; i++)
+		max_len = nl(arr[i]) > max_len ? max_len = nl(arr[i]) : max_len;
+	for (size_t pl = 0; pl < max_len; pl++) {
+		std::vector<int> base[10];	// создание и заполнения массива элементами по цифре в их текущем разряде
+		for (size_t i = 0; i < len; i++) {
+			base[digt(arr[i], pl)].push_back(arr[i]);
+		}
+		size_t cntr = 0;
+		for (size_t i = 0; i < 10; i++) {	// запись по новому порядку в исходный массив
+			for (size_t j = 0; j < base[i].size(); j++) {
+				arr[cntr] = base[i][j];
+				cntr++;
+			}
+		}
+	}
 
 
-if __name__ == '__main__':
-    evaluate(input("Введите пример: "))
+	for (size_t i = 0; i < len; i++)
+		std::cout << arr[i] << ' ';
+	std::cout << std::endl;
+
+	return 0;
+}
